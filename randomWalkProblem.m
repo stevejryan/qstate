4e5988b9f7a8091d159bc719a20d1c76eed9cdc1
stevejryan <stevejryan@gmail.com>
2017-03-20 17:24:38 -0400

    First commit, created .m file

---
+++ randomWalkProblem.m
@@ -0,0 +1,15 @@
+% randomWalkProblem.m
+% Steve Ryan, started 3/20/2017
+
+% Problem Description:  Starting from the origin O, a particle moves 
+% randomly +/- 1 step along each axis of an N-dimensional space. The 
+% probabilities of stepping +1 or -1 step along each axis are each 50% and 
+% are statistically independent of each other. After a step is taken, the 
+% move is repeated from the new position, and so on. If the walk continues 
+% forever, what is the probability that the particle ever returns to the 
+% origin O if N = 1, 2, and 3? Please solve this problem numerically and 
+% send us your code and a brief summary of your solution and findings.
+% Hint: The problem has an analytical solution. Please use the analytical 
+% solution to verify or discuss your numerical approach. [Link to 
+% analytical solution: 
+% http://mathworld.wolfram.com/PolyasRandomWalkConstants.html ]
