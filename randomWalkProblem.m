e6743a06e2864267408b2f02f91505bf4fb196d8
stevejryan <stevejryan@gmail.com>
2017-03-21 14:43:21 -0400

    Updates, first trial of 10,000 run

--- randomWalkProblem.m
+++ randomWalkProblem.m
@@ -19,6 +19,7 @@
 % as an approximation for infinity.  It loses a lot of the nuance of the
 % formal solution, and will consistently underestimate the probability, 
 % but for a numerical solution it should work reasonably well.
+
 % I'm going to start by generating a matrix of numbers randomly
 % assigned to be either -1 or +1, representing the movements along this 
 % axis.  Then I'll generate a parallel list which represents a running
@@ -28,17 +29,20 @@
 
 tic
 
-trials = 10;
+trials = 10000;
 
-returnToOriginThree = zeros(1,trials);
+returnToOriginXYZ = zeros(1,trials);
 returnToOriginXY = zeros(1,trials);
 returnToOriginYZ = zeros(1,trials);
 returnToOriginZX = zeros(1,trials);
+returnToOriginX = zeros(1,trials);
+returnToOriginY = zeros(1,trials);
+returnToOriginZ = zeros(1,trials);
     
 for j=1:trials
 
     % generate list of movements for each dimension
-    X = 2*floor(2*rand(10000,1)) - 1;
+    X = 2*floor(2*rand(10000,1)) - 1; % composed entirely of -1 and +1
     Y = 2*floor(2*rand(10000,1)) - 1;
     Z = 2*floor(2*rand(10000,1)) - 1;
     
@@ -65,14 +69,52 @@ for j=1:trials
     
     % The sum of the logical operation will be non-zero if the particle 
     % returns to origin in all axes simultaneously, and zero if it never
-    % returns.
-    returnToOriginThree(j) = sum(allAtOrigin==3);
-    returnToOriginXY(j) = sum(xyAtOrigin==2);
-    returnToOriginYZ(j) = sum(yzAtOrigin==2);
-    returnToOriginZX(j) = sum(zxAtOrigin==2);
-    returnToOriginX(j) = sum(xAtOrigin==1);
-    returnToOriginY(j) = sum(yAtOrigin==1);
-    returnToOriginZ(j) = sum(zAtOrigin==1);
+    % returns.  The addition of the '~= 0' will convert non-zero values to
+    % 1, simplifying the next step.
+    returnToOriginXYZ(j) = sum(allAtOrigin==3) ~= 0;
+    returnToOriginXY(j) = sum(xyAtOrigin==2) ~= 0;
+    returnToOriginYZ(j) = sum(yzAtOrigin==2) ~= 0;
+    returnToOriginZX(j) = sum(zxAtOrigin==2) ~= 0;
+    returnToOriginX(j) = sum(xAtOrigin==1) ~= 0;
+    returnToOriginY(j) = sum(yAtOrigin==1) ~= 0;
+    returnToOriginZ(j) = sum(zAtOrigin==1) ~= 0;
 end
 
+% in the numerator, the sum of each matrix represents the number of trials
+% where the particle returned to Origin.  In the denominator is the sum of
+% the lenghts of each matrix, representing the number of total trials.  The
+% ratio then becomes effectively a probability.
+probabilityInOneDimension = sum([sum(returnToOriginX),sum(returnToOriginY),...
+    sum(returnToOriginZ)])/sum([length(returnToOriginX),length(returnToOriginY),length(returnToOriginZ)])
+
+probabilityInTwoDimensions = sum([sum(returnToOriginXY),sum(returnToOriginYZ),...
+    sum(returnToOriginZX)])/sum([length(returnToOriginXY),length(returnToOriginYZ),length(returnToOriginZX)])
+
+probabilityInThreeDimensions = sum(returnToOriginXYZ)/length(returnToOriginXYZ)
+
 toc
+
+% output
+% I ran a simulation of 10 thousand trials, and got the following outputs
+% p(1) = 99.17 %
+% p(2) = 73.74 %
+% p(3) = 27.51 %
+%
+% As expected, all values are underestimating what we know to be the actual
+% values from the analytical solution.
+%
+% Advantages of this approach: The simulation approach can be used in
+% situations where we don't have an analytical solution to check against
+% for the right answers.  
+%
+% Disadvantages: In this case, since we're dealing with an infinite
+% process, a large number of simulations doesn't capture the full behavior
+% of the infinite process, as demonstrated most visibly in the p(2) case.
+% We can know ahead of time that the results will underestimate the true
+% probability, but we can't really know the magnitude of that error without
+% the analytical solution to compare to.
+%
+% Alternatives:  Since we have the analytical solution, we could implement
+% a numerical approximation of that integral function.  I would note that
+% the integral is still over an infinite domain, and so it may suffer some
+% of the same limitations seen in the simulation approach.
