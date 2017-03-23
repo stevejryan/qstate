a004badfae73aeb511490ca19b78691be4eda525
Steve <stevejryan@gmail.com>
2017-03-23 10:15:33 -0400

    added imports

--- dataSetTwo.py
+++ dataSetTwo.py
@@ -4,4 +4,12 @@ Created on Tue Mar 21 17:10:40 2017
 
 @author: Steve
 """
+import pandas as pd
+import numpy as np
+import matplotlib as plt
 
+dsTwo = pd.read_csv(filepath_or_buffer='/home/steve/GitHub/qstate/sampleFeatureMatrix.csv', engine='python')
+
+for i, group in dsTwo.groupby('Concentration1'):
+#    plt.figure()
+    group.plot(x='Concentration1', y='Feature1')
