254349b22d614ee1e46d6b355f75e498ce1df0c5
stevejryan <stevejryan@gmail.com>
2017-03-24 17:49:21 -0400

    Updating matlab to 2016b for better table support

--- dataSetTwo.m
+++ dataSetTwo.m
@@ -4,37 +4,29 @@
 % from inspecting the data manually in Excel, Plates 1 & 2 are RT, plates 3
 % & 4 are 32 C.  Most FOVs have multiple measurements, but all are at the
 % same drug concentration, selected from [0, 0.03, 0.1, 0.3, 1, 3, 10, 30].
-% There are 32 fields of view in each plate.  
-% Some rows are empty, or just have empty cells in certain columns.  We'll
-% have to cull those at the appropriate time.
-clear all;
-data = readtable('sampleFeatureMatrix.csv');
+% There are 32 fields of view in each plate.  Looks like there is some 
+% missing data here.  9 rows missing entirely, another 132 with at least 1 
+% missing value.
 
-%% Manual Data Segmentation
+% I'm interested in the underlying structure of the data.  I'm going to
+% import the data, clean it up, sort it, and process it with a hierarchical
+% clustering algorithm.  I'll perform the hierarchical clustering without 
+% the Temperature, Concentration, Plate Number, and FOV features.
+% Afterwards, we can compare the resulting dendrogram to these features
+% (particularly Temperature and Concentration) to see if these experimental
+% variables resulted in meaningful clustering of the data.  This is a nice
+% way to do a first-pass inspection of the data set looking for underlying
+% structure and validating the importance of the experimental variables.
 
-% There are for sure more graceful ways to do this in R, but in Matlab I
-% have traditionally generated my own tables in ways that didn't need
-% segmentation like this to examine, so I've never learned this corner of
-% Matlab.  Hence, I'm doing it the ugly way =)
+% Hierarchical clustering doesn't deal well with missing data.  There are a
+% variety of approaches to trying to replace missing data.  Here, as a
+% first pass, I'm going to take the conservative approach of just removing 
+% rows with missing data.  A more advanced approach would be to use the
+% complete portions of the data set to impute reasonable substitutions for 
+% missing values.  After it's cleaned up, I'm going to sort it, just to
+% make it easier to identify what data ends up in which clusters.
 
-dataRT = data(ismember(data.Temperature,'RT','rows'),:);
-data32 = data(ismember(data.Temperature,'32 C','rows'),:);
-
-dataRT_0   = dataRT(ismember(dataRT.Concentration1,'0 uM','rows'),:);
-dataRT_003 = dataRT(ismember(dataRT.Concentration1,'0.03 uM','rows'),:);
-dataRT_01  = dataRT(ismember(dataRT.Concentration1,'0.1 uM','rows'),:);
-dataRT_03  = dataRT(ismember(dataRT.Concentration1,'0.3 uM','rows'),:);
-dataRT_1   = dataRT(ismember(dataRT.Concentration1,'1 uM','rows'),:);
-dataRT_3   = dataRT(ismember(dataRT.Concentration1,'3 uM','rows'),:);
-dataRT_10  = dataRT(ismember(dataRT.Concentration1,'10 uM','rows'),:);
-dataRT_30  = dataRT(ismember(dataRT.Concentration1,'30 uM','rows'),:);
-
-data32_0   = data32(ismember(data32.Concentration1,'0 uM','rows'),:);
-data32_003 = data32(ismember(data32.Concentration1,'0.03 uM','rows'),:);
-data32_01  = data32(ismember(data32.Concentration1,'0.1 uM','rows'),:);
-data32_03  = data32(ismember(data32.Concentration1,'0.3 uM','rows'),:);
-data32_1   = data32(ismember(data32.Concentration1,'1 uM','rows'),:);
-data32_3   = data32(ismember(data32.Concentration1,'3 uM','rows'),:);
-data32_10  = data32(ismember(data32.Concentration1,'10 uM','rows'),:);
-data32_30  = data32(ismember(data32.Concentration1,'30 uM','rows'),:);
+rawData = readtable('sampleFeatureMatrix.csv');
+lessData = rawData(:,1:31);
+cleanData = rmmissing(lessData);
 
